dee7f02fffa1c91477bde79201f09389e417f2c8
Neon-099 <Emmannatividad099@gmail.com>
2026-02-07 18:59:28 +0800

    use authprovider for the auth to double check auth

--- web-client/src/main.jsx
+++ web-client/src/main.jsx
@@ -8,18 +8,22 @@ import Message from './page/Message'
 import Delivery from './page/Delivery'
 import Auth from './page/Auth'
 import ContactUs from './page/landing/ContactUs'
+import { AuthProvider } from './contexts/AuthProvider'
+
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-   <Router>
-        <Routes>
-          <Route path='/' element={<Landing />} />
-          <Route path='/auth' element={<Auth />} />
-          <Route path='/contact' element={<ContactUs />} />
-          <Route path='/home' element={<Home />} />
-          <Route path='/messages' element={<Message />} />
-          <Route path='/delivery' element={<Delivery />} />
-        </Routes>
-      </Router>
+    <AuthProvider>
+        <Router>
+          <Routes>
+            <Route path='/' element={<Landing />} />
+            <Route path='/auth' element={<Auth />} />
+            <Route path='/contact' element={<ContactUs />} />
+            <Route path='/home' element={<Home />} />
+            <Route path='/messages' element={<Message />} />
+            <Route path='/delivery' element={<Delivery />} />
+          </Routes>
+        </Router>
+    </AuthProvider>
   </StrictMode>,
 )
