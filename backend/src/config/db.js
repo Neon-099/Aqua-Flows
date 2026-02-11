9826dd65bc02c76704c789404d3140f92bdf304b
Neon-099 <Emmannatividad099@gmail.com>
2026-02-11 18:36:07 +0800

    add debugging tool

--- backend/src/config/db.js
+++ backend/src/config/db.js
@@ -4,6 +4,46 @@
 import mongoose from 'mongoose';
 import { env } from './env.js';
 
+const enableDbTiming = env.DB_TIMING === 'true' || env.NODE_ENV !== 'production';
+
+if (enableDbTiming) {
+  const timingPlugin = (schema) => {
+    const startTimer = function () { this._timingStart = Date.now(); };
+    const endTimer = function (op) {
+      const ms = Date.now() - (this._timingStart || Date.now());
+      const model = this?.model?.modelName || this?.constructor?.modelName || 'Unknown';
+      console.log(`[DB] ${model}.${op} ${ms}ms`);
+    };
+
+    schema.pre('save', function (next) { startTimer.call(this); next(); });
+    schema.post('save', function () { endTimer.call(this, 'save'); });
+
+    const queryOps = [
+      'find',
+      'findOne',
+      'findOneAndUpdate',
+      'findOneAndDelete',
+      'findOneAndReplace',
+      'updateOne',
+      'updateMany',
+      'count',
+      'countDocuments',
+      'deleteOne',
+      'deleteMany',
+    ];
+
+    queryOps.forEach((op) => {
+      schema.pre(op, function (next) { startTimer.call(this); next(); });
+      schema.post(op, function () { endTimer.call(this, op); });
+    });
+
+    schema.pre('aggregate', function (next) { startTimer.call(this); next(); });
+    schema.post('aggregate', function () { endTimer.call(this, 'aggregate'); });
+  };
+
+  mongoose.plugin(timingPlugin);
+}
+
 export const connectDB = async () => {
   try {
     const conn = await mongoose.connect(env.DATABASE_URI);
